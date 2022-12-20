784f0c4c5039a5e7b85f855da8725747b9c3e591
Ram sai prasanna teja karnam <118451996+Ramsai64@users.noreply.github.com>
2022-12-20 13:51:44 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Table
+{
+  public static void main(String args[])
+   {
+     Scanner s= new Scanner(System.in);
+     for(int i=0;i>10;i++)
+      {
+        System.out.println("2*i="+2*i);
+       }
+  }
+}
